b4b4ab596dad88c8da65a08666af593433fc1169
MiguelDotL <miguel.d.lozano@gmail.com>
2016-11-21 10:41:01 -0500

    Add @classmethods and @staticmethods examples

--- oop_python/classes.py
+++ oop_python/classes.py
@@ -1,47 +1,81 @@
 class Employee:
 
-    raisePercent = 1.04
-    employeeCount = 0
+    raise_percent = 1.04
+    employee_count = 0
 
-    def __init__(self, firstName, lastName, salary, company='company'):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.email = str(firstName + '.' + lastName + '@' + company + '.com').lower()
+    def __init__(self, first_name, last_name, salary, company='company'):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = str(first_name + '.' + last_name + '@' + company + '.com').lower()
         self.salary = salary
 
-        Employee.employeeCount += 1
+        Employee.employee_count += 1
 
-    def fullName(self):
-        return self.firstName + ' ' + self.lastName
-        # return '{} {}'.format(self.firstName, self.lastName)
+    def full_name(self):
+        return self.first_name + ' ' + self.last_name
+        # return '{} {}'.format(self.first_name, self.last_name)
 
-    def applyRaise(self):
-        self.salary = float(self.salary * self.raisePercent)
+    def apply_raise(self):
+        self.salary = float(self.salary * self.raise_percent)
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_percent = amount
+
+    @classmethod
+    def from_string(cls,empl_str):
+        first_name, last_name, salary = empl_str.split('-')
+        return cls(first_name, last_name, salary)
+
+    @staticmethod
+    def is_workday(day):
+        return False if day.weekday() >= 5 else True
+
 
 
 joe = Employee("Joe", "Schmoe", 55000, "compuserve")
 bill = Employee("Bill", "Lumburgh", 155000, "inatek")
 
-# joe.firstName   = "Joe"
-# joe.lastName    = "Schmoe"
+empl_str_1 = 'Jon-Doe-70000'
+empl_str_2 = 'Mark-Sklar-90000'
+empl_str_3 = 'Mr-Potato-40000'
+
+print("--- MAKING A NEW EMPLOYEE --- ")
+new_employee1 = Employee.from_string(empl_str_1)
+print(new_employee1.first_name)
+
+# joe.first_name   = "Joe"
+# joe.last_name    = "Schmoe"
 # joe.email       = "jschmoe69@compuserve.com"
 # joe.salary      = 55000
 #
-# bill.firstName   = "Bill"
-# bill.lastName    = "Lumburgh"
+# bill.first_name   = "Bill"
+# bill.last_name    = "Lumburgh"
 # bill.email       = "billyboy@inatek.com"
 # bill.salary      = 155000
 
 print(joe.email)
-print(joe.fullName())
+print(joe.full_name())
 print(bill.email)
 
 
 print(joe.__dict__)
 
-print(Employee.raisePercent)
-print(joe.raisePercent)
-print(bill.raisePercent)
+Employee.set_raise_amount(1.05)
 
-joe.raisePercent = 1.6
+print(Employee.raise_percent)
+print(joe.raise_percent)
+print(bill.raise_percent)
+
+joe.raise_percent = 1.06
 print(joe.__dict__)
+
+
+
+# ---- static methods ----
+
+import datetime
+my_date = datetime.date(2016, 7 ,10)
+
+print('Today s a weekday:')
+print (Employee.is_workday(my_date))
